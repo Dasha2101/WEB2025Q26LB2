631ed24067492ce29413e068f5ff8690afd47108
Клапнева Диана Дмитриевна <klapneva.diana@yandex.ru>
2025-11-23 01:39:24 +0300

    filter-btn

--- list.js
+++ list.js
@@ -89,7 +89,7 @@ class ToDo {
         const filterToggleBtn = document.createElement('button');
         filterToggleBtn.id = 'filterToggleBtn';
         filterToggleBtn.textContent = 'Фильтры';
-        filterToggleBtn.className = 'sort-btn';
+        filterToggleBtn.className = 'filter_btn';
         controlPanel.appendChild(filterToggleBtn);//
 
         const filterDropdown = document.createElement('div');
