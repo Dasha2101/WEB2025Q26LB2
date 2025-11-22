d4f846246acad0901f08007ea1fd5578a4e5bbeb
Клапнева Диана Дмитриевна <klapneva.diana@yandex.ru>
2025-11-22 23:10:00 +0300

    title&favicon

--- list.js
+++ list.js
@@ -61,12 +61,6 @@ class ToDo {
         searchInput.placeholder = 'Поиск задачи';
         searchInput.className = 'search-input';
 
-        const clearSearchBtn = document.createElement('button');
-        clearSearchBtn.id = 'clearSearchBtn';
-        clearSearchBtn.textContent = '×';
-        clearSearchBtn.className = 'clear-search-btn';
-        clearSearchBtn.style.display = 'none';//пока прячем
-
         searchGroup.appendChild(searchInput);
         searchGroup.appendChild(clearSearchBtn);
         container.appendChild(searchGroup);
